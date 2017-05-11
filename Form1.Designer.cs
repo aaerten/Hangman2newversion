59fef4bfd6eb400164deea8e63ae742159a8cc00
Alp Erten <alperten95@gmail.com>
2017-05-11 14:27:58 +0300

    new version 2

---
+++ Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Hangman3
+{
+    partial class homepage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl_word = new System.Windows.Forms.Label();
+            this.pcb_man = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbl_spoken = new System.Windows.Forms.Label();
+            this.btn_try = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbl_category = new System.Windows.Forms.Label();
+            this.strt_btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_man)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbl_word
+            // 
+            this.lbl_word.AutoSize = true;
+            this.lbl_word.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_word.Location = new System.Drawing.Point(113, 105);
+            this.lbl_word.Name = "lbl_word";
+            this.lbl_word.Size = new System.Drawing.Size(0, 23);
+            this.lbl_word.TabIndex = 0;
+            this.lbl_word.Visible = false;
+            // 
+            // pcb_man
+            // 
+            this.pcb_man.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pcb_man.Location = new System.Drawing.Point(652, 17);
+            this.pcb_man.Name = "pcb_man";
+            this.pcb_man.Size = new System.Drawing.Size(258, 530);
+            this.pcb_man.TabIndex = 1;
+            this.pcb_man.TabStop = false;
+            this.pcb_man.Visible = false;
+            this.pcb_man.Paint += new System.Windows.Forms.PaintEventHandler(this.pcb_man_Paint);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lbl_spoken);
+            this.panel1.Controls.Add(this.btn_try);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.lbl_category);
+            this.panel1.Controls.Add(this.strt_btn);
+            this.panel1.Controls.Add(this.pcb_man);
+            this.panel1.Controls.Add(this.lbl_word);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(929, 561);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // lbl_spoken
+            // 
+            this.lbl_spoken.AutoSize = true;
+            this.lbl_spoken.Location = new System.Drawing.Point(39, 393);
+            this.lbl_spoken.Name = "lbl_spoken";
+            this.lbl_spoken.Size = new System.Drawing.Size(0, 13);
+            this.lbl_spoken.TabIndex = 6;
+            this.lbl_spoken.Visible = false;
+            // 
+            // btn_try
+            // 
+            this.btn_try.Location = new System.Drawing.Point(224, 285);
+            this.btn_try.Name = "btn_try";
+            this.btn_try.Size = new System.Drawing.Size(75, 23);
+            this.btn_try.TabIndex = 5;
+            this.btn_try.Text = "Try";
+            this.btn_try.UseVisualStyleBackColor = true;
+            this.btn_try.Visible = false;
+            this.btn_try.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(117, 285);
+            this.textBox1.MaxLength = 1;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.Visible = false;
+            // 
+            // lbl_category
+            // 
+            this.lbl_category.AutoSize = true;
+            this.lbl_category.Location = new System.Drawing.Point(39, 29);
+            this.lbl_category.Name = "lbl_category";
+            this.lbl_category.Size = new System.Drawing.Size(35, 13);
+            this.lbl_category.TabIndex = 3;
+            this.lbl_category.Text = "label1";
+            this.lbl_category.Visible = false;
+            // 
+            // strt_btn
+            // 
+            this.strt_btn.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.strt_btn.Location = new System.Drawing.Point(406, 285);
+            this.strt_btn.Name = "strt_btn";
+            this.strt_btn.Size = new System.Drawing.Size(91, 38);
+            this.strt_btn.TabIndex = 2;
+            this.strt_btn.Text = "START";
+            this.strt_btn.UseVisualStyleBackColor = true;
+            this.strt_btn.Click += new System.EventHandler(this.strt_btn_Click);
+            // 
+            // homepage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(953, 585);
+            this.Controls.Add(this.panel1);
+            this.Name = "homepage";
+            this.Text = "Homepage";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.homepage_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.pcb_man)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_word;
+        private System.Windows.Forms.PictureBox pcb_man;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button strt_btn;
+        private System.Windows.Forms.Label lbl_category;
+        private System.Windows.Forms.Button btn_try;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbl_spoken;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Hangman3
+{
+    //we define a class in here.//********************************CLASS******************************************//
+
+    public partial class homepage : Form
+    {
+        public homepage()
+        {
+            InitializeComponent();
+        }
+        string[] letters = { "a", "b", "c", "ç", "d", "e", "f", "g", "ğ", "h", "ı", "i", "j", "k", "l", "m", "n", "o", "ö", "p", "r", "s", "ş", "t", "u", "ü", "v", "y", "z", "x", "q", "w" };
+        Hangman_operations cl_hangman;
+        string word;
+        string[] selected_word;
+        int trial_right=7;
+        int remaining_right;
+        int width, height;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            set_location();
+            width =pcb_man.Width ;
+            height = pcb_man.Height;
+            remaining_right = trial_right;
+            selected_word = new string[0];
+            cl_hangman = new Hangman_operations();
+        }
+        private void select_word()
+        {
+
+            Hangman_operations.New_Word our_word_ = cl_hangman.take_word();
+            lbl_category.Text =" This is a/an  " + our_word_.category;
+            word = our_word_.word;
+            foreach (char letter in word.ToCharArray())
+            {
+                lbl_word.Text = lbl_word.Text + "_ ";
+            }
+            strt_btn.Visible = false;
+
+
+        }
+
+        private void strt_btn_Click(object sender, EventArgs e)
+        {
+            pcb_man.Visible = true;
+            textBox1.Visible = true;
+            lbl_spoken.Visible = true;
+            lbl_word.Visible = true;
+            lbl_category.Visible = true;
+            btn_try.Visible = true;
+            select_word();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string data = textBox1.Text;
+            bool found = false;
+            bool spoken = false;
+            foreach(var item in selected_word)
+            {
+                if(data.ToLower() == item.ToLower())
+                {
+                    spoken = true;
+                    MessageBox.Show("You have said this letter before");
+                }
+            }
+            if (!spoken) { 
+            for (int i =0;i < letters.Length; i++)
+            {
+                if (data.ToLower() == letters[i])
+                {
+                    found = true;
+                    find_the_word(data);
+                    break;
+                }
+            }
+            if (!found)
+            {
+                MessageBox.Show("Write the valid value");
+            }
+            update();
+            }
+            int index = lbl_word.Text.IndexOf(" ");
+            if (index == -1)
+            {
+                next_stage();
+            }
+        }
+        private void find_the_word(string letter)
+        {
+            bool check_correct = false;
+            char[] array = word.ToCharArray();
+            for(int a=0;a<array.Length;a++)
+            {
+                if(array[a].ToString() != "")
+                {
+                    if (letter == array[a].ToString().ToLower())
+                    {
+                        check_correct = true;
+                        lbl_word.Text = lbl_word.Text.Remove(a * 2, 1);
+                        lbl_word.Text = lbl_word.Text.Insert(a * 2, letter).ToUpper();
+                        
+                    }
+                }
+            }
+            if (!check_correct)
+            {
+                remaining_right--;
+                pcb_man.Invalidate();
+            }
+            add_letter(letter);
+        }
+        private void update()
+        {
+            lbl_spoken.Text = "";
+            foreach(string item in selected_word)
+            {
+                lbl_spoken.Text = lbl_spoken.Text + item + " ";
+            }
+        }
+
+        private void pcb_man_Paint(object sender, PaintEventArgs e)
+        {
+            Pen pencil = new Pen(Color.Black, 12);
+            if (remaining_right != -1)
+            {
+                if (remaining_right < trial_right)
+                {
+                    e.Graphics.DrawLine(pencil, width / 10, height / 15, width / 10, height / 15 * 14);
+                    e.Graphics.DrawLine(pencil, width / 10, height / 15, width / 2, height / 15);
+                }
+                if (remaining_right < trial_right - 1)
+                {
+                    e.Graphics.DrawLine(pencil, width / 2, height / 15, width / 2, height / 15 * 3);
+                    e.Graphics.DrawEllipse(pencil, width / 2 - width / 20, height / 5, width / 10, height / 10);
+                }
+                if (remaining_right < trial_right - 2)
+                {
+                    e.Graphics.DrawLine(pencil, width / 2, height / 10 * 3, width / 2, height / 10 * 6);
+                }
+                if (remaining_right < trial_right - 3)
+                {
+                    e.Graphics.DrawLine(pencil, width / 2, height / 10 * 3, width / 2 + width / 10, height / 10 * 3 + height / 5);
+                }
+                if (remaining_right < trial_right - 4)
+                {
+                    e.Graphics.DrawLine(pencil, width / 2, height / 10 * 3, width / 2 - width / 10, height / 10 * 3 + height / 5);
+                }
+                if (remaining_right < trial_right - 5)
+                {
+                    e.Graphics.DrawLine(pencil, width / 2, height / 10 * 6, width / 2 + width / 10, height / 10 * 6 + height / 10);
+                }
+                if (remaining_right < trial_right - 6)
+                {
+                    e.Graphics.DrawLine(pencil, width / 2, height / 10 * 6, width / 2 - width / 10, height / 10 * 6 + height / 10);
+
+                }
+            }
+            else {game_over(); }
+
+        }
+
+        private void add_letter(string letter)
+        {
+            string[] a = new string[selected_word.Length + 1];
+           for(int i = 0; i < selected_word.Length; i++)
+            {
+                a[i] = selected_word[i];
+            }
+            a[a.Length - 1] = letter;
+            selected_word = a;
+        }
+        private void next_stage()
+        {
+            clean();
+            select_word();
+        }
+        private void game_over()
+        {
+            MessageBox.Show("Game is over. Correct word is : " + word);
+            strt_btn.Visible = true;
+            textBox1.Visible = false;
+            btn_try.Visible = false;
+            clean();
+            pcb_man.Visible = false;
+        }
+
+        private void homepage_SizeChanged(object sender, EventArgs e)
+        {
+
+        }
+        public void set_location()
+        {
+            int width, height;
+            width = this.Width;
+            height = this.Height;
+            panel1.Width = width;
+            panel1.Height = height;
+            panel1.Location = new Point(0, 0);
+            pcb_man.Width = width / 9 * 4;
+            pcb_man.Height = height / 4 * 3;
+            pcb_man.Location = new Point(width - pcb_man.Width - 10, 10);
+            lbl_category.Location = new Point(10, 10);
+            lbl_word.Location = new Point(10, 50);
+            lbl_spoken.Location = new Point(10, 120);
+            strt_btn.Size = new System.Drawing.Size(width / 3, height / 10);
+            textBox1.Location = new Point(10, 90);
+            strt_btn.Location = new Point(width / 2 - strt_btn.Width / 2, height / 2 - strt_btn.Height / 2);
+            btn_try.Location = new Point(120, 90);
+            lbl_spoken.Location = new Point(10, height / 2);
+
+
+
+
+        }
+        private void clean()
+        {
+            selected_word = new string[0];
+            remaining_right = trial_right;
+            pcb_man.Invalidate();
+            lbl_category.Text = "";
+            lbl_word.Text = "";
+            lbl_spoken.Text = "";
+            textBox1.Text = "";
+        }
+    }
+    class Hangman_operations
+    {
+        private List<New_Word> New_Word_List;
+        private List<New_Word> Word_List;
+        public Hangman_operations()
+        {
+            take_data_folder();
+
+        }
+        public struct New_Word
+        {
+            public string word;
+            public string category;
+        }
+        public New_Word take_word()
+        {
+            if (Word_List.Count == 0)
+            {
+                Word_List = New_Word_List;
+            }
+            Random rndm = new Random();
+            int a = rndm.Next(0, Word_List.Count);
+            New_Word our_word = Word_List[a];
+            return our_word;
+        }
+
+        private New_Word Create_New_Word(string word,string category)
+        {
+            New_Word our_word = new New_Word();
+            our_word.word = word;
+            our_word.category = category;
+            return our_word;
+        }
+        private void take_data_folder()
+        {
+            
+            New_Word_List = new List<New_Word>();
+            StreamReader read = new StreamReader(@"data.txt", Encoding.Default);
+            string data;
+            while ((data = read.ReadLine()) != null)
+            {
+                if (data != "")
+                {
+                    New_Word_List.Add(Create_New_Word(data.Split(',')[0], data.Split(',')[1]));
+                }
+            }
+            Word_List = New_Word_List;
+
+
+        }
+
+
+
+
+
+    }
+
+}
